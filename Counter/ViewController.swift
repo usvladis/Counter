6b89e0a95c0b5d6d787e2423223a7af2b276065c
usvladis <usvladis@gmail.com>
2024-01-11 21:09:52 +0300

    Сделана базовая задача. Реализован счетчик с одной кнопкой, при нажатии на которую счет увеличивается на 1. При запуске приложения изначально отображается 0.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var buttonTap: UIButton!
+    public var count: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        counterLabel.text = "\(count)"
     }
 
-
+    @IBAction func buttonTapped(_ sender: Any) {
+        count += 1
+        counterLabel.text = "\(count)"
+    }
+    
 }
 
